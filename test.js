777d2d9e43e1a76890e4ffa6486cc6258ee96769
Anthony Beaumont <dev.xan105@outlook.be>
2020-02-07 20:14:00 +0700

    1.1.0
    - remove rwlock dep (I think can safely remove it)
    - add group and progress tag option
    - (optional) Use native WinRT API instead of PowerShell if NodeRT native modules are installed

--- test.js
+++ test.js
@@ -3,10 +3,12 @@
 const toast = require('./toast.js');
 
 toast({
+  disableWinRT: true,
   appID: "Microsoft.XboxApp_8wekyb3d8bbwe!Microsoft.XboxApp",
   title: "Dummy",
   message: "Hello World",
   icon: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/480/winner.jpg",
+  attribution: "Achievement",
   timeStamp: "1568710924",
   button: [
     {text: "1", onClick: "bingmaps:?q=sushi"},
@@ -16,10 +18,12 @@ toast({
     {text: "5", onClick: "bingmaps:?q=sushi"},
     {text: "6", onClick: "bingmaps:?q=sushi"}
   ],
+  silent: false,
   audio: "ms-winsoundevent:Notification.Achievement",
   progress:{
     header: "up",
     footer: "down",
-    percent: 88
+    percent: 92,
+    tag: "id0"
   }
 }).then(()=>{console.log("ok")}).catch((err)=>{console.log(err)});
