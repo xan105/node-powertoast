ce6834fb48b0ab37508c050c4794fd5587449d60
Anthony Beaumont <dev.xan105@outlook.be>
2021-08-11 14:01:37 +0700

    simplify

--- test/demo.ps1
+++ test/demo.ps1
@@ -1,18 +1,14 @@
 ﻿(Get-Process -Id $pid).PriorityClass = 'High'
 $Version = $PSVersionTable.PSVersion
 if($Version.Major -gt 7 -or ($Version.Major -eq 7 -and $Version.Minor -ge 1)){
-  $Assemblies = @(
-    [pscustomobject]@{name = 'Microsoft.Windows.SDK.NET.Ref';version = '10.0.20348.20';files = @('lib\WinRT.Runtime.dll';'lib\Microsoft.Windows.SDK.NET.dll')}
-  )
-  ForEach ($Lib in $Assemblies){
-    if (!(Get-Package -Name $Lib.name -ErrorAction SilentlyContinue)){
-      Install-Package -Name $Lib.name -MinimumVersion $Lib.version -ProviderName NuGet -Source 'https://www.nuget.org/api/v2' -Force -Scope CurrentUser
-    }
-    ForEach ($File in $Lib.files) {
-      $Source = Split-Path -Path (Get-Package -Name $Lib.name).Source
-      $FilePath = Join-Path -Path $Source -ChildPath $File
-      Add-Type -Path $FilePath -ErrorAction Stop
-    }
+  $Assembly = [pscustomobject]@{name = 'Microsoft.Windows.SDK.NET.Ref';version = '10.0.20348.20';files = @('lib/WinRT.Runtime.dll';'lib/Microsoft.Windows.SDK.NET.dll')}
+  if (!(Get-Package -Name $Assembly.name -ErrorAction SilentlyContinue)){
+    Install-Package -Name $Assembly.name -MinimumVersion $Assembly.version -ProviderName NuGet -Source 'https://www.nuget.org/api/v2' -Force -Scope CurrentUser
+  }
+  $Source = Split-Path -Path (Get-Package -Name $Assembly.name).Source
+  ForEach ($File in $Assembly.files) {
+    $FilePath = Join-Path -Path $Source -ChildPath $File
+    Add-Type -Path $FilePath -ErrorAction Stop
   }
 }else{
   [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
